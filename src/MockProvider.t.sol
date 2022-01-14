9e99d00b4f538a891a70281367151d2596ac4f7d
Lisandro Corbalan <lmcorbalan@gmail.com>
2022-01-14 10:29:45 -0300

    Add givenSelectorReturnResponse

--- src/MockProvider.t.sol
+++ src/MockProvider.t.sol
@@ -58,6 +58,32 @@ contract MockProviderTest is DSTest {
         emit log_bytes(responseReceived);
     }
 
+    function test_SetResponse_For_Selector(bytes memory params) public {
+        bytes4 selector = bytes4(0x72aa1899);
+        bytes memory response = hex"5678";
+        mockProvider.givenSelectorReturnResponse(
+            selector,
+            MockProvider.ReturnData({success: true, data: response}),
+            false
+        );
+
+        bytes memory query = abi.encodePacked(selector, params);
+
+        // solhint-disable-next-line
+        (bool okReceived, bytes memory responseReceived) = address(mockProvider)
+            .call(query);
+
+        assertTrue(okReceived, "Should not fail doing a call");
+        assertEq(
+            keccak256(response),
+            keccak256(responseReceived),
+            "Returned response should match"
+        );
+
+        emit log_bytes(response);
+        emit log_bytes(responseReceived);
+    }
+
     function test_SetResponse_ToFail_ForQuery() public {
         bytes memory query = hex"1234";
         mockProvider.givenQueryReturnResponse(
